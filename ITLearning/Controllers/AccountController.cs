2fb032ceb13e0fc9b4dfd66d4172c5afc7b1fb23
Przemek S <przemek.smyrdek@gmail.com>
2015-09-13 22:33:32 +0200

    account basic methods and generic layout (todo)

---
+++ ITLearning/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace ITLearning.Frontend.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult Unauthorized()
+        {
+            return View();
+        }
+    }
+}
