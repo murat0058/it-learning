fdc4d1cc0771f87af3af5bb0a238c659536fb9b0
Przemek <przemek.smyrdek@gmail.com>
2015-10-17 12:44:05 +0200

    profile widget mock

--- ITLearning/wwwroot/_references.js
+++ ITLearning/wwwroot/_references.js
@@ -2,6 +2,7 @@
 /// <reference path="../gulpfile.js" />
 /// <reference path="dist/js/jquery-package.js" />
 /// <reference path="src/js/header/header-ui.js" />
+/// <reference path="src/js/home/profile-widget-ui.js" />
 /// <reference path="src/js/landing/jquery.scrolly.min.js" />
 /// <reference path="src/js/landing/main.js" />
 /// <reference path="src/js/landing/util.js" />
