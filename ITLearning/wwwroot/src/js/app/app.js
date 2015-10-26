591808ee08d6a92e78c56a75f267fa29400c6c10
Przemek <przemek.smyrdek@gmail.com>
2015-10-26 20:48:43 +0100

    task-list directive started, changes in password hasher

---
+++ ITLearning/wwwroot/src/js/app/app.js
@@ -0,0 +1,8 @@
+﻿(function () {
+
+    angular
+        .module('app', [
+            'app.tasks'
+        ]);
+
+})();
---
+++ ITLearning/wwwroot/src/js/app/tasks/taskList.directive.js
@@ -0,0 +1,74 @@
+﻿(function () {
+
+    /// <itl-task-list no-of-tasks="5"></itl-task-list>
+
+    angular
+        .module('app.tasks')
+        .directive('itlTaskList', itlTaskList);
+
+    function itlTaskList() {
+        var directive = {
+            templateUrl: 'src/js/app/tasks/templates/task-list.html',
+            restrict: 'E',
+            scope: {
+                noOfTasks: '@'
+            },
+            controller: TasksController,
+            controllerAs: 'vm',
+            bindToController: true,
+            link: link
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+
+            //TODO tooltip
+
+        };
+    };
+
+    function TasksController() {
+
+        var vm = this;
+
+        vm.tasks = [
+            {
+                taskId: 0,
+                groupId: 1,
+                language: 'C#',
+                taskName: 'Zadanie testowe',
+                groupName: 'Grupa testowa',
+                isTaskCompleted: true,
+                statusTooltip: 'Zadanie wykonane',
+                style: {
+                    'background-color': '#ff8d00'
+                }
+            },
+            {
+                taskId: 1,
+                groupId: 1,
+                language: 'C#',
+                taskName: 'Zadanie testowe',
+                groupName: 'Grupa testowa',
+                isTaskCompleted: false,
+                statusTooltip: 'Zadanie wykonane',
+                style: {
+                    'background-color': '#21b2a6'
+                }
+            },
+            {
+                taskId: 2,
+                groupId: 1,
+                language: 'JAVA',
+                taskName: 'Zadanie testowe',
+                groupName: 'Grupa testowa',
+                isTaskCompleted: true,
+                statusTooltip: 'Zadanie wykonane',
+                style: {
+                    'background-color': '#018ee0'
+                }
+            }
+        ];
+    };
+
+})();
