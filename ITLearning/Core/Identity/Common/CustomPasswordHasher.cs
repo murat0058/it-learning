4547d6ce98d5115abbcc457a01dc65ad2b8350bf
Przemek <przemek.smyrdek@gmail.com>
2015-10-28 17:42:05 +0100

    hasher fix

--- ITLearning/Core/Identity/Common/CustomPasswordHasher.cs
+++ ITLearning/Core/Identity/Common/CustomPasswordHasher.cs
@@ -14,12 +14,17 @@ namespace ITLearning.Frontend.Web.Core.Identity.Common
     {
         public string HashPassword(User user, string password)
         {
-            return Convert.ToBase64String(Encoding.UTF8.GetBytes(password));
+            return ToBase64(password);
         }
 
         public PasswordVerificationResult VerifyHashedPassword(User user, string hashedPassword, string providedPassword)
         {
-            return string.Equals(hashedPassword, providedPassword) ? PasswordVerificationResult.Success : PasswordVerificationResult.Failed;
+            return string.Equals(hashedPassword, ToBase64(providedPassword)) ? PasswordVerificationResult.Success : PasswordVerificationResult.Failed;
+        }
+
+        private string ToBase64(string textToEncode)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(textToEncode));
         }
     }
 }
