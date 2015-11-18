8ad9851b2083eb3b21e57d219585bbc814d5ff58
Adrian Bystrek <adrian.bystrek@gmail.com>
2015-11-18 21:54:51 +0100

    crop images frontend

---
+++ ITLearning/wwwroot/src/js/common/croppic/croppicInit.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    var cropperOptions = {
+        uploadUrl: "UploadImage",
+        cropUrl: "CropImage",
+        //loadPicture: '../Cropped/Przechwytywanie.PNG',
+        onReset: function () {
+            //todo call to controller and reset img
+
+            cropper.destroy();
+
+            var newOptions = {
+                uploadUrl: cropperOptions.uploadUrl,
+                cropUrl: cropperOptions.cropUrl,
+            }
+
+            cropper = new Croppic('croppic', newOptions);
+        }
+    }
+
+    var cropper = new Croppic('croppic', cropperOptions);
+});
