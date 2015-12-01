e3a7e9675925e6ba5abc367f31dcb63d55f66ceb
Przemek <przemek.smyrdek@gmail.com>
2015-12-02 00:04:52 +0100

    new models for groups

---
+++ src/ITLearning.Contract/Data/Model/Groups/GroupWithUsersBasicData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITLearning.Contract.Data.Model.Groups
+{
+    public class GroupWithUsersBasicData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsPrivate{ get; set; }
+        public int NoOfUsers { get; set; }
+    }
+}
---
+++ src/ITLearning.Contract/Data/Model/Groups/UserGroupData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITLearning.Contract.Data.Model.Groups
+{
+    public class UserGroupData
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public int GroupId { get; set; }
+        public string GroupName { get; set; }
+    }
+}
