06a53e1c5bd29d87dcab820539f5e8278d39283f
Przemek <przemek.smyrdek@gmail.com>
2015-12-03 22:44:42 +0100

    groups layout fixed, group user directive

---
+++ src/ITLearning.Frontend.Web/wwwroot/src/js/app/Groups/groupUsers.directive.js
@@ -0,0 +1,40 @@
+﻿/**
+* @desc Manage users in given group directive
+* @example <itl-group-users group-id="5"></itl-group-users>
+*/
+(function () {
+
+    angular
+        .module('app.groups')
+        .directive('itlGroupUsers', itlGroupUsers);
+
+    function itlGroupUsers() {
+
+        var directive = {
+            templateUrl: '/src/js/app/Groups/templates/group-users.html',
+            restrict: 'E',
+            transclude: true,
+            scope: {
+                groupId: '@'
+            },
+            controller: GroupUsersController,
+            controllerAs: 'groupUsersVm',
+            bindToController: true
+        };
+
+        return directive;
+    }
+
+    GroupUsersController.$inject = ['groupsService', 'loadingIndicatorService'];
+
+    function GroupUsersController(groupsService, loadingIndicatorService) {
+
+        var groupUsersVm = this;
+
+        var loadingMessage = "Pobieram dane...";
+
+        groupUsersVm.loadingIndicator = loadingIndicatorService.getIndicator(loadingMessage);
+
+    }
+
+})();
