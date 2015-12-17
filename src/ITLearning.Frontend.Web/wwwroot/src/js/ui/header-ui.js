a93f7e7458cdd7925f47f8292fbcaa045990f5d2
Przemek <przemek.smyrdek@gmail.com>
2015-12-17 23:36:46 +0100

    layout improvements

--- src/ITLearning.Frontend.Web/wwwroot/src/js/ui/header-ui.js
+++ src/ITLearning.Frontend.Web/wwwroot/src/js/ui/header-ui.js
@@ -1,32 +1,26 @@
 ﻿$(function () {
 
-    var $toggleBtn = $('.menu-title'),
+    var $toggleBtn = $('.menu-title-toggle'),
         $menu = $('nav.menu'),
-        $icon = $('.menu-icon'),
         $overlay = $('.overlay'),
         animation = 'ease-in',
         animationStates = {
             0: {
                 position: 0,
-                cssClass: 'fa fa-angle-double-right',
                 overlayVisible: true
             },
             1: {
                 position: -250,
-                cssClass: 'fa fa-angle-double-left',
                 overlayVisible: false
             }
-        },
+        }
         currentState = 0;
 
     var toggleMenuVisibility = function () {
 
         var newState = animationStates[currentState % 2],
             newPosition = newState.position,
-            newClass = newState.cssClass,
             isOverlayVisible = newState.overlayVisible;
-
-        $icon.removeClass().addClass(animationStates[currentState % 2].cssClass);
 
         var overlayAnimationSpeed = 150,
             requiredOpacity = 0.8;
@@ -44,7 +38,6 @@
 
         currentState++;
     };
-
+    
     $toggleBtn.click(toggleMenuVisibility);
-
 });
